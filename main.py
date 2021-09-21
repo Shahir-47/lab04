9c78537d971e91dd7f8980378de8a549910af112
shahir1775 <67286712+shahir1775@users.noreply.github.com>
2021-09-21 20:30:27 +0000

    st03.1 - Drawing Hill

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ def drawBG(width, height, color, window):
 		background.setFill(color)
 		background.setOutline(color)
 		background.draw(window)
+
+def hill(width, height, color, window):
+	snowHill = Polygon(Point(0, 0), Point(width, 0), Point(0, height))
+	snowHill.setFill(color)
+	snowHill.setOutline(color)
+	snowHill.draw(window)
 		
 maxHeight = 500
 maxWidth = 500
 bgColor = color_rgb(190, 255, 255)
+hillColor = color_rgb(255, 255, 255)
 
 snowWind = GraphWin("Winter is coming", maxHeight, 500)
 snowWind.setCoords(0, 0, maxHeight, 500)
 
-BlueBackground = drawBG(maxWidth, maxHeight, bgColor, snowWind)
+blueBackground = drawBG(maxWidth, maxHeight, bgColor, snowWind)
+
+hillSide = hill(maxWidth, maxHeight, hillColor, snowWind)
 
 snowWind.getMouse()
 snowWind.close()
