c0a60230d5110b1388240d1c49fcaf1fc79c8134
shahir1775 <67286712+shahir1775@users.noreply.github.com>
2021-09-21 20:17:05 +0000

    st03 - BG set

--- main.py
+++ main.py
@@ -3,8 +3,21 @@
 
 from graphics import *
 
-snowWind = GraphWin("Winter is coming", 500, 500)
-snowWind.setCoords(0, 0, 500, 500)
+
+def drawBG(width, height, color, window):
+		background = Rectangle(Point(0, 0),Point(width, height))
+		background.setFill(color)
+		background.setOutline(color)
+		background.draw(window)
+		
+maxHeight = 500
+maxWidth = 500
+bgColor = color_rgb(190, 255, 255)
+
+snowWind = GraphWin("Winter is coming", maxHeight, 500)
+snowWind.setCoords(0, 0, maxHeight, 500)
+
+BlueBackground = drawBG(maxWidth, maxHeight, bgColor, snowWind)
 
 snowWind.getMouse()
 snowWind.close()
