bdc65c71f3fed374893116c028a92d2e0dd1235f
shahir1775 <67286712+shahir1775@users.noreply.github.com>
2021-09-21 19:38:19 +0000

    Drew the graphics window

---
+++ main.py
@@ -0,0 +1,10 @@
+#snowThem.py, a graphics program
+#Shahir & Maia
+
+from graphics import *
+
+snowWind = GraphWin("Winter is coming", 500, 500)
+snowWind.setCoords(0, 0, 500, 500)
+
+snowWind.getMouse()
+snowWind.close()
