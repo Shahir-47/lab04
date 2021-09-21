e7c2d80a6fccd7c27848fd3d63af28340e44dbff
shahir1775 <67286712+shahir1775@users.noreply.github.com>
2021-09-21 20:46:43 +0000

    st04 - draws circle

--- main.py
+++ main.py
@@ -9,13 +9,26 @@ def drawBG(width, height, color, window):
 		background.setFill(color)
 		background.setOutline(color)
 		background.draw(window)
+		hillSide = hill(maxWidth, maxHeight, hillColor, snowWind)
+
+def drawCirc(x, y, radius, color, window):
+	 head = Circle(Point(x,y), radius)
+	 head.setFill(color)
+	 head.setOutline(color)
+	 head.draw(window)
+
 
 def hill(width, height, color, window):
 	snowHill = Polygon(Point(0, 0), Point(width, 0), Point(0, height))
 	snowHill.setFill(color)
 	snowHill.setOutline(color)
 	snowHill.draw(window)
-		
+	head = drawCirc(startX, startY, headRadius, headColor, snowWind)
+
+headRadius = 6
+startX = 20
+startY = 480
+headColor = color_rgb(200, 200, 200)
 maxHeight = 500
 maxWidth = 500
 bgColor = color_rgb(190, 255, 255)
@@ -26,7 +39,6 @@ snowWind.setCoords(0, 0, maxHeight, 500)
 
 blueBackground = drawBG(maxWidth, maxHeight, bgColor, snowWind)
 
-hillSide = hill(maxWidth, maxHeight, hillColor, snowWind)
 
 snowWind.getMouse()
 snowWind.close()
